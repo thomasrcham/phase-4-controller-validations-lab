dd08ebc037067d3e6c54052f441653f4711878b4
thomasrcham <guseldorph@gmail.com>
2022-11-08 13:00:36 -0600

    commit tree 8808a85f0ac7af1bc64d21aca6276954bf3ff25d

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,4 @@
 class AuthorsController < ApplicationController
-  
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +6,19 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: {
+             errors: invalid.record.errors
+           },
+           status: :unprocessable_entity
   end
 
   private
-  
+
   def author_params
     params.permit(:email, :name)
   end
-  
 end
